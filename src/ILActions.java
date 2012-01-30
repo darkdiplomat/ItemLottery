8a390b63da408c9b14ad6908ee32a56751ec84b8
darkdiplomat <darkdiplomat@visualillusionsent.net>
2012-01-30 02:03:09 -0600

    Added proper README and added License info

--- src/ILActions.java
+++ src/ILActions.java
@@ -1,6 +1,26 @@
 import java.util.List;
 import java.util.Random;
 
+/**
+* ItemLottery v1.x
+* Copyright (C) 2011 Visual Illusions Entertainment
+* @author darkdiplomat <darkdiplomat@hotmail.com>
+*
+* This file is part of ItemLottery.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+*/
 
 public class ILActions {
 	Server server = etc.getServer();
@@ -36,7 +56,7 @@ public class ILActions {
 			drawing = false;
 		}
 		else if(Tickets.size() < ILD.MTS){
-			server.messageAll("[§dILotto§f]§c Not enought tickets sold. Starting new round!");
+			server.messageAll("[§dILotto§f]§c Not enough tickets sold. Starting new round!");
 			ILD.log.info("[ILotto] Not enought tickets sold. Starting new round!");
 			drawing = false;
 		}
@@ -50,13 +70,20 @@ public class ILActions {
 					addItem(player.getInventory(), ILD.WinItem.getItemId(), ILD.WinItem.getDamage(), ILD.WinItem.getAmount());
 					WinItem = ILD.WinItem;
 				}
+				else{
+					ILD.addItemWaiting(name, ILD.WinItem);
+					WinItem = ILD.WinItem;
+				}
 			}
 			else{
-				if(player.isConnected()){
-					List<Item> RandItems = ILD.getRandItemList();
-					int randI = new Random().nextInt(RandItems.size());
-					WinItem = RandItems.get(randI);
+				List<Item> RandItems = ILD.getRandItemList();
+				int randI = new Random().nextInt(RandItems.size());
+				WinItem = RandItems.get(randI);
+				if(player != null && player.isConnected()){
 					addItem(player.getInventory(), WinItem.getItemId(), WinItem.getDamage(), WinItem.getAmount());
+				}
+				else{
+					ILD.addItemWaiting(name, WinItem);
 				}
 			}
 			String Itemname = WinItem.itemType.name();
@@ -68,7 +95,10 @@ public class ILActions {
 			server.messageAll("[§dILotto§f]§b There was in total §e" + Tickets.size() + " §btickets sold.");
 			ILD.log.info("[ILotto] Winner was " + name + ". Item won was "+WinItem.getAmount()+" "+Itemname+" Starting new round!");
 			if (ILD.UTE && (etc.getLoader().getPlugin("TwitterEvents") != null) && (etc.getLoader().getPlugin("TwitterEvents").isEnabled())){
-				etc.getLoader().callCustomHook("tweet", new Object[] { ILD.Tweet.replace("<P>", name).replace("<A>", String.valueOf(WinItem.getAmount()).replace("<I>", Itemname))});
+				String Tweet = ILD.Tweet.replace("<P>", name);
+				Tweet = Tweet.replace("<A>", String.valueOf(WinItem.getAmount()));
+				Tweet = Tweet.replace("<I>", Itemname);
+				etc.getLoader().callCustomHook("tweet", new Object[] { Tweet });
 			}
 			ILD.clearTicketList();
 			drawing = false;
@@ -210,6 +240,5 @@ public class ILActions {
 				break;
 			}
 		}
-		return;
 	}
 }
